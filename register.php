e4b22a014b437d0a23373233bbeccf1b0561ece5
Curious-Ujjwal <ujjwal24111999@gmail.com>
2019-10-05 05:19:54 +0530

    header and minor background

--- register.php
+++ register.php
@@ -285,7 +285,9 @@
       <nav class="navbar navbar-expand-md tm-navbar" id="tmNav">
         <div class="container">
           <div class="tm-next">
-              <a href="#hero" class="navbar-brand">NESS</a>
+              <a href="#hero" class="navbar-brand">
+              	<img src="logo.svg" height="70px" width="70px">
+              </a>
           </div>
 
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -581,52 +583,63 @@
 
     <!-- Contact -->
     <section id="contact" class="tm-section-pad-top tm-parallax-2">
-      <div class="container tm-container-contact">
-        <div class="row">
-            <div class="col-12">
-                <h2 class="mb-4 tm-section-title">Contact Us</h2>
-                <div class="mb-5 tm-underline">
-                  <div class="tm-underline-inner"></div>
-                </div>
+      	<div class="container tm-container-contact">
+        	<div class="row">
+            	<div class="col-12">
+                	<h2 class="mb-4 tm-section-title">Contact Us</h2>
+           	        <div class="mb-5 tm-underline">
+               		   <div class="tm-underline-inner"></div>
+                	</div>
+                <!--
                 <p class="mb-5">
                   Nullam tincidunt, lacus a suscipit luctus, elit turpis tincidunt dui,
                   non tempus sem turpis vitae lorem. Maecenas eget odio in sapien ultrices
                   viverra vitae vel leo. Curabitur at elit eu risus pharetra pellentesque
                   at at velit.
                 </p>
-            </div>
+            	-->
+            	</div>
 
-            <div class="col-sm-12 col-md-6 d-flex align-items-center tm-contact-item">
-              <a href="tel:0100200340" class="tm-contact-item-link">
-                  <i class="fas fa-3x fa-phone mr-4"></i>
-                  <span class="mb-0">010-020-0340</span>
-              </a>
-            </div>
-            <div class="col-sm-12 col-md-6 d-flex align-items-center tm-contact-item">
-              <a href="mailto:info@company.co" class="tm-contact-item-link">
-                  <i class="fas fa-3x fa-envelope mr-4"></i>
-                  <span class="mb-0">info@company.co</span>
-              </a>
-            </div>
-            <div class="col-sm-12 col-md-6 d-flex align-items-center tm-contact-item">
-              <a href="https://www.google.com/maps" class="tm-contact-item-link">
-                  <i class="fas fa-3x fa-map-marker-alt mr-4"></i>
-                  <span class="mb-0">Location on Maps</span>
-              </a>
-            </div>
-            <div class="col-sm-12 col-md-6 d-flex align-items-center tm-contact-item">
-              <form action="" method="get">
-                <input name="email" type="email" placeholder="Subscribe your email" class="tm-input" required />
-                <button type="submit" class="btn tm-btn-submit">Submit</button>
-              </form>
-            </div>
+            	<div class="col-sm-12 col-md-6 d-flex align-items-center tm-contact-item">
+              		<a href="tel:0100200340" class="tm-contact-item-link">
+                  		<i class="fas fa-3x fa-phone mr-4"></i>
+                  		<span class="mb-0">010-020-0340</span>
+              		</a>
+            	</div>
+            	<div class="col-sm-12 col-md-6 d-flex align-items-center tm-contact-item">
+              		<a href="mailto:info@company.co" class="tm-contact-item-link">
+                 	 	<i class="fas fa-3x fa-envelope mr-4"></i>
+                  		<span class="mb-0">info@company.co</span>
+              		</a>
+            	</div>
+            	<div class="col-sm-12 col-md-6 d-flex align-items-center tm-contact-item">
+              		<a href="https://www.google.com/maps" class="tm-contact-item-link">
+                  		<i class="fas fa-3x fa-map-marker-alt mr-4"></i>
+                  		<span class="mb-0">Location on Maps</span>
+              		</a>
+            	</div>
+            	<div class="col-sm-12 col-md-6 d-flex align-items-center tm-contact-item">
+              		<form action="" method="get">
+                		<input name="email" type="email" placeholder="Subscribe your email" class="tm-input" required />
+                		<button type="submit" class="btn tm-btn-submit">Submit</button>
+              		</form>
+            	</div>
+
+            <div class="footer-social d-flex align-items-center">
+									<a href="https://www.facebook.com/alcheringaiitg"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                    <a href="https://twitter.com/alcheringaiitg"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                    <a href="https://www.youtube.com/user/iitgalcher"><i class="fa fa-youtube" aria-hidden="true"></i></a>
+                                    <a href="https://plus.google.com/+alcheringa"><i class="fa fa-google" aria-hidden="true"></i></a>
+                                    <a href="https://www.linkedin.com/company/alcheringaiitguwahati/"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                                    <a href="https://www.instagram.com/alcheringaiitg/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                                    <a href="https://in.pinterest.com/alcheringaiitg/"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+								</div>
         </div>
       </div>
       <footer class="text-center small tm-footer">
           <p class="mb-0">
-            Copyright &copy; 2019 Company Name
-
-            - Design: <a rel="nofollow" href="https://templatemo.com" class="tm-footer-link">Template Mo</a>
+            Copyright &copy; 2020 Alcheringa<br>
+            Developed by Team WebOps
           </p>
         </footer>
     </section>
